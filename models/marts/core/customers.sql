8d9904f11631fd27ed2e3d54c0b911f9d9c89142
awmiesch@gmail.com <awmiesch@gmail.com>
2022-09-02 17:36:16 +0000

    updated marts/core

--- models/marts/core/customers.sql
+++ models/marts/core/customers.sql
@@ -2,7 +2,7 @@ with customers as (
     select * from {{ ref('stg_customers')}}
 ),
 orders as (
-    select * from {{ ref('stg_orders')}}
+    select * from {{ ref('customer_orders')}}
 ),
 customer_orders as (
     select
